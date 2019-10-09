caadff4ec4dee0d6d0eee8a10d8d70da3556667a
Ciro Continisio <ciro@unity3d.com>
2019-10-09 11:40:17 +0200

    First commit post-Unite

---
+++ Assets/AppearDisappearController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearDisappearController : MonoBehaviour
+{
+	private Renderer target;
+	private bool isVisible = true;
+
+	void Awake()
+	{
+		target = GetComponent<Renderer>();
+	}
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			if(isVisible)
+			{
+				//Play effect backwards
+				target.material.SetFloat("_PlayingForward", 0f);
+				target.material.SetFloat("_AppearFXTime", Time.time);
+			}
+			else
+			{
+				//Play effect forward
+				target.material.SetFloat("_PlayingForward", 1f);
+				target.material.SetFloat("_AppearFXTime", Time.time);
+			}
+
+			isVisible = !isVisible;
+		}
+
+
+		if(Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			//Play one-time effect
+			target.material.SetFloat("_ShakeFXTime", Time.time);
+		}
+    }
+}
